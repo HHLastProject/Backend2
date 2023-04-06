cceda39c24af0bad8bed76d55335a68b163b4730
song33ztpgg <sasasa7737@gmail.com>
2023-04-06 21:29:20 +0900

    [test]해더로 값 보내기

--- routes/login.route.js
+++ routes/login.route.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const LoginController = require("../controllers/login.controller");
 const loginController = new LoginController();
 
-router.get("/loginKakao", loginController.loginKakao);
+router.post("/loginKakao", loginController.loginKakao);
+// router.get("/loginKakao", loginController.loginKakao);
 router.get("/loginNaver", loginController.loginNaver);
 router.post("/logintest", loginController.logintest);
 module.exports = router;
