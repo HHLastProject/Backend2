d5eac006b07b21c5a2ff7c6c0ec2208068e1820d
song33ztpgg <sasasa7737@gmail.com>
2023-04-07 15:35:14 +0900

    [test]네이버,카카오톡 유저정보를 jwt로 보내주기

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,17 +1,18 @@
 const LoginServices = require("../services/login.service");
+const jwt = require("jsonwebtoken");
+
 class LoginController {
 
   loginServices = new LoginServices();
-
 
   loginKakao = async (req, res, next) => {
     try {
       // const { code } = req.query;
       const {code} = req.body
 
-      console.log("code");
-      console.log(code);
-      console.log("================================");
+      // console.log("code");
+      // console.log(code);
+      // console.log("================================");
        const resultToken = await this.loginServices.getKakaoToken(code);
 
       console.log("resultToken");
@@ -20,15 +21,24 @@ class LoginController {
 
       const resultUser = await this.loginServices.getKaKaoUserInfo(resultToken);
 
-      // console.log("resultUser");
-      // console.log(resultUser);
-      // console.log("================================");
+      console.log("resultUser");
+      console.log(resultUser);
+      console.log("================================");
 
       // //유저정보 저장
       // // await this.loginServices.saveUser(resultUser);
 
-      res.set("authorization", `${resultToken}`);
-      res.status(200).json({"msg" : "토큰발급"});
+      const userData = jwt.sign(
+        { 
+          id :  resultUser.id,
+        },
+        "key",
+        );
+
+      res.set("authorization", `Bearer ${userData}`);  
+      res.status(200).json({ "유저정보 jwt": " id 를 보냈습니다"});
+      // res.set("authorization", `Bearer ${resultToken}`);
+      // res.status(200).json({"msg" : resultToken});
     } catch (err) {
       console.log(err);
       res.status(400).json({ errorMsg: "예기치 못한 오류가 발생했습니다" });
@@ -42,15 +52,38 @@ class LoginController {
       // const { code } = req.query;
       const { code } = req.body
 
-      const resultToken = await this.loginServices.getNaverToken(code);
+      // const resultToken = await this.loginServices.getNaverToken(code);
+     
       // const resultUser = await this.loginServices.getNaverUserInfo(resultToken);
+      
+
+     
+
+
+      // 현재 발급 받은 토큰이 유저 정보가 담긴 토큰인지 확인
+      const resultUser = await this.loginServices.getNaverUserInfo(code);
+      
+      console.log("resultUser");
+      console.log(resultUser);
+      console.log("================================");
 
       //유저정보저장
       // this.loginServices.saveUser(resultUser);
 
-      res.set("authorization", `${resultToken}`)
-      res.status(200).json({"msg" : "토큰발급"});
-    } catch (err) {
+      // res.set("authorization", `${resultToken}`)
+      // res.status(200).json({"msg" : "토큰발급"});
+
+      const userData = jwt.sign(
+        { 
+          id :  resultUser.id,
+        },
+        "key",
+        );
+
+
+      res.set("authorization", `Bearer ${userData}`);  
+      res.status(200).json({ "유저정보 jwt": " id 를 보냈습니다"});
+      } catch (err) {
       console.log(err);
       res.status(400).json({ errorMsg: "예기치 못한 오류가 발생했습니다" });
     }
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const userRouter = require("./user.route");
+
 const shopRouter = require("./shop.route");
 const adminRouter = require('./admin.route');
 const kakaoRouter = require('./kakao.route');
 const naverRouter = require('./naver.route');
 const loginRouter = require('./login.route');
 
-router.use("/api/users", userRouter);
 router.use("/api/shop", shopRouter);
 router.use('/api/admin', adminRouter);
 router.use('/api/login', loginRouter);
