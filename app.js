3362c4fd5ee6aaa73c4703a5ce715fa695797454
song33ztpgg <sasasa7737@gmail.com>
2023-04-15 16:01:10 +0900

    [fix]오타수정

--- app.js
+++ app.js
@@ -24,58 +24,58 @@ const routes = require("./routes");
 /////////////////////////////////////////////////////////////
 
 
-const session = require('express-session');
+// const session = require('express-session');
 
-app.use(session({
-  secret: 'YOUR_SESSION_SECRET',
-  resave: false,
-  saveUninitialized: true,
-}));
+// app.use(session({
+//   secret: 'YOUR_SESSION_SECRET',
+//   resave: false,
+//   saveUninitialized: true,
+// }));
 
-const KAKAO_CLIENT_ID = process.env.KAKAO_CLIENT_ID;
-const KAKAO_REDIRECT_URI = process.env.KAKAO_REDIRECT_URI
+// const KAKAO_CLIENT_ID = process.env.KAKAO_CLIENT_ID;
+// const KAKAO_REDIRECT_URI = process.env.KAKAO_REDIRECT_URI
 
-app.get('/kakaologin', (req, res) => { 
-  //  (1,2)과정  (31,32번 자료를 보내며) 인가코드를 주세요 
-  const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
-  res.redirect(kakaoLoginUrl);
-});
+// app.get('/kakaologin', (req, res) => { 
+//   //  (1,2)과정  (31,32번 자료를 보내며) 인가코드를 주세요 
+//   const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+//   res.redirect(kakaoLoginUrl);
+// });
 
 
-// //로컬에서 네이버 로그인을 할때 사용
-// //1~3번 과정이다 
-/*
-const NAVER_CLIENT_ID =  process.env.NAVER_CLIENT_ID
-const NAVER_REDIRECT_URI = encodeURI(process.env.NAVER_REDIRECT_URI); 
-app.get("/naverlogin", function (req, res) {
-  var api_url ="https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=" +
-  NAVER_CLIENT_ID + "&redirect_uri=" + NAVER_REDIRECT_URI;
-  res.end(
-    "<a href='" +
-      api_url +
-      "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>"
-  );
-});
+// // //로컬에서 네이버 로그인을 할때 사용
+// // //1~3번 과정이다 
+// /*
+// const NAVER_CLIENT_ID =  process.env.NAVER_CLIENT_ID
+// const NAVER_REDIRECT_URI = encodeURI(process.env.NAVER_REDIRECT_URI); 
+// app.get("/naverlogin", function (req, res) {
+//   var api_url ="https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=" +
+//   NAVER_CLIENT_ID + "&redirect_uri=" + NAVER_REDIRECT_URI;
+//   res.end(
+//     "<a href='" +
+//       api_url +
+//       "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>"
+//   );
+// });
 
+// // api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
+// //      + NAVER_CLIENT_ID + '&client_secret=' + client_secret + '&redirect_uri=' + NAVER_REDIRECT_URI + '&code=' + code + '&state=' + state;
 // api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
-//      + NAVER_CLIENT_ID + '&client_secret=' + client_secret + '&redirect_uri=' + NAVER_REDIRECT_URI + '&code=' + code + '&state=' + state;
-api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
-     + NAVER_CLIENT_ID + '&redirect_uri=' + NAVER_REDIRECT_URI + '&code=' + code + '&state=' + state;*/
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const NAVER_CLIENT_ID =  process.env.NAVER_CLIENT_ID
-const NAVER_REDIRECT_URI = encodeURI(process.env.NAVER_REDIRECT_URI); 
-app.get("/naverlogin", function (req, res) {
-  var api_url ="https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=" +
-  NAVER_CLIENT_ID + "&redirect_uri=" + NAVER_REDIRECT_URI;
-  res.end(
-    "<a href='" +
-      api_url +
-      "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>"
-  );
-});
+//      + NAVER_CLIENT_ID + '&redirect_uri=' + NAVER_REDIRECT_URI + '&code=' + code + '&state=' + state;*/
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// const NAVER_CLIENT_ID =  process.env.NAVER_CLIENT_ID
+// const NAVER_REDIRECT_URI = encodeURI(process.env.NAVER_REDIRECT_URI); 
+// app.get("/naverlogin", function (req, res) {
+//   var api_url ="https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=" +
+//   NAVER_CLIENT_ID + "&redirect_uri=" + NAVER_REDIRECT_URI;
+//   res.end(
+//     "<a href='" +
+//       api_url +
+//       "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>"
+//   );
+// });
 
- api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
-      + NAVER_CLIENT_ID + '&redirect_uri=' + NAVER_REDIRECT_URI 
+//  api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
+//       + NAVER_CLIENT_ID + '&redirect_uri=' + NAVER_REDIRECT_URI 
 
 
 
