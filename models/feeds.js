eb2fe7fbdb8f49988cad4e706d68a45610d7b152
chanwoongpark97 <clue97@naver.com>
2023-04-15 10:43:26 +0900

    [fix] DB 일부 수정, 마이페이지 일부 수정

--- models/feeds.js
+++ models/feeds.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.hasMany(models.Tegs, {
+      this.hasMany(models.Tags, {
         sourceKey: "feedId",
         foreignKey: "FeedId",
       });
