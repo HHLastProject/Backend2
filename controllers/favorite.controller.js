375a89bd742a1c395218a281a941bab6b63a5632
song33ztpgg <sasasa7737@gmail.com>
2023-04-27 20:25:12 +0900

    [feat]폴더 생성,폴더 삭제

--- controllers/favorite.controller.js
+++ controllers/favorite.controller.js
@@ -3,7 +3,6 @@ const FavoriteService = require("../services/favorite.service");
 class favoriteController {
   favoriteService = new FavoriteService();
 
-
   //즐겨찾기 리스트 보여주기
   listFavorite = async (req, res, next) => {
     const { userId } = res.locals.user;
@@ -13,196 +12,123 @@ class favoriteController {
     //Scrap 데이터에 가계정보 가져오기
     const result2 = await this.favoriteService.findOneShops(myAllScrap, userId);
 
-   
     let folderList = await this.favoriteService.findAllFolder(myAllScrap);
     //수정
-    // let folderList = await Folders.findAll({}) 
-    // folderList = folderList.map((value)=> { 
+    // let folderList = await Folders.findAll({})
+    // folderList = folderList.map((value)=> {
     //   return value.folderName
     // })
 
-    res.status(200).json({ scrapList: result2, folderList});
+    res.status(200).json({ scrapList: result2, folderList });
   };
-
-  
 
   makeFolders = async (req, res, next) => {
     const { userId } = res.locals.user;
-    let {folderList} = req.body;
-   
+    let { folderList } = req.body;
+
     //내가 Scrap한 데이터 가져오기
     const myAllScrap = await this.favoriteService.findAllScrap(userId);
- 
 
     //나의 스크랩한 shopId 전체
-    let myAllScrapShopId = myAllScrap.map((value)=> { 
-      return value.shopId
-    })
+    let myAllScrapShopId = myAllScrap.map((value) => {
+      return value.shopId;
+    });
 
     //나의 스크랩한 scrapId전체
-    let myAllScrapId = myAllScrap.map((value)=> { 
-      return value.scrapId
-    })
+    let myAllScrapId = myAllScrap.map((value) => {
+      return value.scrapId;
+    });
 
     //내가 만든 folder 전체 가져오기
     // const myAllFolder = await this.favoriteService.findAllFolder()
 
     //추가한 shopList중에 즐겨 찾기를 않하면 파일 생성을 할 수 없도록 구현
-    for(let i = 0; i < folderList.length;i++){
-      
+    for (let i = 0; i < folderList.length; i++) {
       // let isMyFolderName = myAllFolder.indexOf(folderList[i].folderName)
       // if(isMyFolderName != -1){
       //   return res.send("중복된 가게 이름이 있습니다")
       // }
 
-      for(let j =0; j < folderList[i].shopList.length; j++){
-        let result = folderList[i].shopList[j].shopId
-        let isMyScrapbyshopList = myAllScrapShopId.indexOf(result)
-        if(isMyScrapbyshopList  == -1){
-          return res.send("즐겨찾기에 추가된 목록이 아닙니다")
+      for (let j = 0; j < folderList[i].shopList.length; j++) {
+        let result = folderList[i].shopList[j].shopId;
+        let isMyScrapbyshopList = myAllScrapShopId.indexOf(result);
+        if (isMyScrapbyshopList == -1) {
+          return res.send("즐겨찾기에 추가된 목록이 아닙니다");
         }
       }
     }
 
     //기존 폴더 삭제
-    await this.favoriteService.folderDelete(myAllScrapId)
+    await this.favoriteService.folderDelete(myAllScrapId);
 
     //폴더 생성 (스크랩ID,폴더이름 )
-    for(let i = 0; i < folderList.length ; i++){
+    for (let i = 0; i < folderList.length; i++) {
       const createFolder = await this.favoriteService.folderCreate(
         myAllScrap[i].scrapId,
-        userId,  
-        folderList[i].folderName    
+        userId,
+        folderList[i].folderName
       );
 
-      
-      const folder = createFolder.folderId
-     
+      const folder = createFolder.folderId;
+
       //내용물 가게작성(폴더Id,가게Id)
-      for (let j = 0; j <folderList[i].shopList.length ; j++) {
-        let seletedShopId = folderList[i].shopList[j].shopId
-        await this.favoriteService.listCreate(folder,seletedShopId);
+      for (let j = 0; j < folderList[i].shopList.length; j++) {
+        let seletedShopId = folderList[i].shopList[j].shopId;
+        await this.favoriteService.listCreate(folder, seletedShopId);
       }
     }
 
     return res.status(200).json({ msg: "업로드에 성공했습니다" });
   };
 
+  // //빈 폴더 1개 만들기 (폴더는 만들어지는데 하나라도 이름이 다르면 폴더들이 생성이 안됨)
+  // makeFolder = async (req, res, next) => {
 
-  //빈 폴더 1개 만들기
-  makeFolder = async (req, res, next) => {
-    const { userId } = res.locals.user;
-    const {folderName} = req.body
+  //   const { userId } = res.locals.user;
+  //   const { folderList } = req.body;
+  //   //생성된 폴더가 기존 폴더이름과 동일한지 비교 
+  //   let test = await this.favoriteService.compareFolderName(folderList)
 
-    await this.favoriteService.folderCreate(
-      null,  
-      userId,
-      folderName   
-    );
+  //   for (let i = 0; i < folderList.length; i++) {
+  //     await this.favoriteService.folderCreate(null, userId, folderList[i]);
+  //   }
+  //   return res.send("폴더를 생성했습니다");
+  // };
 
-  return res.send("폴더 1개 생성했습니다")
+//빈 폴더 1개 만들기 (폴더는 만들어지는데 하나라도 이름이 다르면 폴더들이 생성이 안됨)
+makeFolder = async (req, res, next) => {
+
+  const { userId } = res.locals.user;
+  const { folderList } = req.body;
+
+  //폴더 리스트에서 동일한 이름이 존재하는지 확인 
+  const FolderListNameResult = await this.favoriteService.compareFolderListName(folderList)
+
+  if(FolderListNameResult){ 
+    return res.status(410).json({"msg" : "동일한 파일 이름을 설정하였습니다" })
   }
 
+  //생성된 폴더가 기존 폴더이름과 동일한지 비교 
+  const FolderNameResult = await this.favoriteService.compareFolderName(folderList)
 
-  // makeFolders = async (req, res, next) => {
-  //   const { userId } = res.locals.user;
-  //   const { shopList } = req.body;
-  //   let total = req.body;
-   
-  //   //내가 Scrap한 데이터 가져오기
-  //    const myAllScrap = await this.favoriteService.findAllScrap(userId);
+  if(FolderNameResult != null){
+    return res.status(410).json({"msg" : "기존에 존재하는 파일과 이름이 동일 합니다"})
+  }
 
-  //   //나의 스크랩한 shopId 전체
-  //   let myAllScrapShopId = myAllScrap.map((value)=> { 
-  //     return value.shopId
-  //   })
+  for (let i = 0; i < folderList.length; i++) {
+    await this.favoriteService.folderCreate(null, userId, folderList[i]);
+  }
 
-  //   let myAllScrapId = myAllScrap.map((value)=> { 
-  //     return value.scrapId
-  //   })
+  return res.status(200).json({"msg" : "폴더를 생성했습니다"});
+};
 
-  //   //추가한 shopList중에 즐겨 찾기를 않하면 파일 생성을 할 수 없도록 구현
-  //   for(let i = 0; i < total.length;i++){
-  //     for(let j =0; j < total[i].shopList.length; j++){
-  //       let result = total[i].shopList[j].shopId
-  //       let isMyScrapbyshopList = myAllScrapShopId.indexOf(result)
-  //       if(isMyScrapbyshopList  == -1){
-  //         return res.send("즐겨찾기에 추가된 목록이 아닙니다")
-  //       }
-  //     }
-  //   }
+deleteFolder = async (req, res, next) => {
+  let { folderId } = req.params
 
-  //   //기존 폴더 삭제
-  //   await this.favoriteService.folderDelete(myAllScrapId)
+  await this.favoriteService.deleteFolder(folderId)
 
-  //   //폴더 생성 (스크랩ID,폴더이름 )
-  //   for(let i = 0; i < total.length ; i++){
-  //     const createFolder = await this.favoriteService.folderCreate(
-  //       myAllScrap[i].scrapId,  
-  //       total[i].folderName    
-  //     );
-
-  //     const folder = createFolder.folderId
-     
-  //     //내용물 가게작성(폴더Id,가게Id)
-  //     for (let j = 0; j <total[i].shopList.length ; j++) {
-  //       let seletedShopId = total[i].shopList[j].shopId
-  //       await this.favoriteService.listCreate(folder,seletedShopId);
-  //     }
-  //   }
-
-  //   return res.status(200).json({ msg: "업로드에 성공했습니다" });
-  // };
-
-  // makeFolders = async (req, res, next) => {
-  //   const { userId } = res.locals.user;
-  //   let total = req.body;
-   
-  //   //내가 Scrap한 데이터 가져오기
-  //    const myAllScrap = await this.favoriteService.findAllScrap(userId);
-
-  //   //나의 스크랩한 shopId 전체
-  //   let myAllScrapShopId = myAllScrap.map((value)=> { 
-  //     return value.shopId
-  //   })
-
-  //   let myAllScrapId = myAllScrap.map((value)=> { 
-  //     return value.scrapId
-  //   })
-
-  //   //추가한 shopList중에 즐겨 찾기를 않하면 파일 생성을 할 수 없도록 구현
-  //   for(let i = 0; i < total.folderList.length;i++){
-  //     for(let j =0; j < total.folderList[i].shopList.length; j++){
-  //       let result = total.folderList[i].shopList[j].shopId
-  //       let isMyScrapbyshopList = myAllScrapShopId.indexOf(result)
-  //       if(isMyScrapbyshopList  == -1){
-  //         return res.send("즐겨찾기에 추가된 목록이 아닙니다")
-  //       }
-  //     }
-  //   }
-
-  //   //기존 폴더 삭제
-  //   await this.favoriteService.folderDelete(myAllScrapId)
-
-  //   //폴더 생성 (스크랩ID,폴더이름 )
-  //   for(let i = 0; i < total.folderList.length ; i++){
-  //     const createFolder = await this.favoriteService.folderCreate(
-  //       myAllScrap[i].scrapId,  
-  //       total.folderList[i].folderName    
-  //     );
-
-  //     const folder = createFolder.folderId
-     
-  //     //내용물 가게작성(폴더Id,가게Id)
-  //     for (let j = 0; j <total.folderList[i].shopList.length ; j++) {
-  //       let seletedShopId = total.folderList[i].shopList[j].shopId
-  //       await this.favoriteService.listCreate(folder,seletedShopId);
-  //     }
-  //   }
-
-  //   return res.status(200).json({ msg: "업로드에 성공했습니다" });
-  // };
-
+  res.status(200).json({"msg" : "폴더를 삭제하였습니다"})
+}
 
 }
 
